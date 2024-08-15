9e11ee2f51e23f0fd2f1231cde8297af187ac02f
admor <adrien_morvan@hotmail.fr>
2024-08-15 11:08:19 +0200

    remove old service starter

--- run_worker_service.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sleep 2 && export OPENAI_TOKEN="xxx" && source /media/amor/Storage/code_dw/cog-Wav2Lip/venv/bin/activate && python3 inference.py --checkpoint_path ./checkpoints/Wav2Lip.pth
